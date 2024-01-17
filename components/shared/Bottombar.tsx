90bd5bcb5fd93a603afad13ae24777467020f49d
waltodd <waltodd@hotmail.com>
2024-01-16 23:11:49 -0500

    bottom bar added

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -12,19 +12,24 @@ export default function Bottombar (){
         <section className="bottombar">
             <div className="bottombar_container">
                 {sidebarLinks.map((link) =>{
+                    const isActive = 
+                    (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route
                     return(
                         <Link
                         href={link.route}
                         key={link.label}
-                        className=""
+                        className={`bottombar_link ${isActive && 'bg-primary-500'}`}
                         >
                             <Image 
                             src={link.imgURL} 
                             alt={link.label} 
                             width={16}
                             height={16}
+                            className="object-contain"
                             />
-                            <p>{link.label}</p>
+                            <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                                {link.label.split(/\s+/)[0]}
+                            </p>
                         </Link>
                         
                     )
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,9 @@
 export default function RightSidebar (){
     return (
-        <div className="rightsidebar">RightSidebar</div>
+        <div className="rightsidebar">
+            <div className="felx flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+            </div>
+        </div>
     )
 }
