596bf6502064c3b69a31eff19081796e1e3efbd2
waltodd <waltodd@hotmail.com>
2024-01-21 18:51:28 -0500

    Add SEO configs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,10 +13,27 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
+  openGraph: {
+    title: 'Threads Clone',
+    description: 'A Next.js 13 Meta Threads application',
+    url: 'https://threads-clone-sepia.vercel.app/',
+    siteName: 'Thread Clone',
+    images: [
+      {
+        url: 'https://utfs.io/f/8e3bf8a8-6f65-4efc-964e-4947f8dde265-k183w6.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://utfs.io/f/b13c3888-5072-46b4-bcbf-d0cdf02f3b07-1u09v.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Desktop image',
+      },
+    ]
+  },
+}
 
-};
 
 export default function RootLayout({
   children,
