bc636a0008e37daa8db3918a5dbf7ad8cda88ec5
waltodd <waltodd@hotmail.com>
2024-01-21 18:58:37 -0500

    fix seo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,15 +15,10 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   openGraph: {
     title: 'Threads Clone',
-    description: 'A Next.js 13 Meta Threads application',
+    description: 'Next.js Thread Clone is a collaborative platform built with Next.js, leveraging UploadThing for seamless file uploads, MongoDB for efficient data storage, and Clerk for secure user authentication. This application enables users to share and discuss files within a threaded environment, providing a modern and responsive user experience.',
     url: 'https://threads-clone-sepia.vercel.app/',
     siteName: 'Thread Clone',
     images: [
-      {
-        url: 'https://utfs.io/f/8e3bf8a8-6f65-4efc-964e-4947f8dde265-k183w6.png', // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
       {
         url: 'https://utfs.io/f/b13c3888-5072-46b4-bcbf-d0cdf02f3b07-1u09v.png', // Must be an absolute URL
         width: 1800,
